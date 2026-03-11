d60c4f572a980c99ff115055c1f2c98d47518f5b
win.cao <win.cao@9tripod.com>
2026-03-11 11:24:32 +0800

    fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.9tripod.com',
+  url: 'https://wincao618.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/', 
+  baseUrl: '/9tripod-docs/', 
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
