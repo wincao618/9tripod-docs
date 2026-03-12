deaaf275149a3f5c7cc039baea4bf958d2bddd0f
win.cao <win.cao@9tripod.com>
2026-03-12 15:13:46 +0800

    modify href

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -203,7 +203,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/wincao618/9tripod-docs',
               },
             ],
           },
